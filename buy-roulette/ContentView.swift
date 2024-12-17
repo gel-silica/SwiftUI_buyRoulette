39e7fe8003611ccdfe33d16cf74c0d0fc088e089
silicagel <ksk19981004@gmail.com>
2024-12-17 22:05:11 +0900

    Initial Commit

---
+++ buy-roulette/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  buy-roulette
+//
+//  Created by silicagel on 2024/12/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ buy-roulette/buy_rouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  buy_rouletteApp.swift
+//  buy-roulette
+//
+//  Created by silicagel on 2024/12/17.
+//
+
+import SwiftUI
+
+@main
+struct buy_rouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
